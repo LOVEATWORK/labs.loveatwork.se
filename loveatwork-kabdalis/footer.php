4c556e00096e895ea5c4ad3c5342a9cce4b221d0
Lars Huring <lars.huring@loveatwork.se>
2014-01-15 16:38:04 +0100

    Added .kit file for index
    Time to create a Wordpress theme from this beginning.

---
+++ loveatwork-kabdalis/footer.php
@@ -0,0 +1,9 @@
+<footer></footer>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+<script src="script/jquery.stellar.min.js"></script>
+<script src="script/iscroll.js"></script>
+<script src="script/scripts.min.js"></script>
+
+</body>
+</html>
---
+++ loveatwork-kabdalis/header.php
@@ -0,0 +1,13 @@
+<?php ?>
+
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
+	<title>Testing the scroller</title>
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,600italic,400,300,600' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Dawning+of+a+New+Day' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/styles.css">
+
+</head>
